ae67bc22f7da9d5feccb4438d1894f893f328757
armorbita <armorbita@gmail.com>
2023-10-25 06:02:46 -0800

    local commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState, useRef, memo } from "react";
 import mainLogo from '../../assets/img/logo_black.png';
 import { FaPhone } from 'react-icons/fa6';
 
 const Header = () => {
+    const [scrollY, setScrollY] = useState(0);
+    const [scrollDirectionUp, setScrollDirectionUp] = useState(false);
+    const headerRef = useRef(null);
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > scrollY) {
+                setScrollDirectionUp(false);
+            } else {
+                setScrollDirectionUp(true);
+            }
+            setScrollY(currentScrollY);
+        };
+
+        if (scrollY > 130 && scrollDirectionUp) {
+            headerRef.current.classList.remove('header_center_mode')
+            headerRef.current.classList.add('header_inline_mode')
+            headerRef.current.classList.remove('header_inline_hide')
+        } else if (scrollY > 130 && !scrollDirectionUp) {
+            headerRef.current.classList.add('header_inline_hide')
+            setTimeout(() => {
+                headerRef.current.classList.add('header_inline_mode')
+                headerRef.current.classList.remove('header_center_mode')
+            }, 500);
+        }
+        else if (scrollY < 2 && !headerRef.current.classList.contains('header_center_mode')) {
+            headerRef.current.classList.add('header_inline_hide')
+            setTimeout(() => {
+                headerRef.current.classList.remove('header_inline_mode')
+                headerRef.current.classList.add('header_center_mode')
+                headerRef.current.classList.remove('header_inline_hide')
+            }, 500);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [scrollY, scrollDirectionUp]);
+
     return (
-        <header className='page_header'>
+        <header ref={headerRef} className="page_header header_center_mode">
             <div className="header_inner">
                 <div className="custom_container">
                     <div className="header_container">
                         <div className="main_logo">
-                            <a href="/#"><img src={mainLogo} alt="main-logo" title="main-logo"/></a>
+                            <a href="/#"><img src={mainLogo} alt="main-logo" title="main-logo" /></a>
                         </div>
                         <ul className="header_list">
                             <li><a href="/#">Home</a></li>
@@ -30,4 +72,4 @@ const Header = () => {
     )
 }
 
-export default Header;
+export default memo(Header);
