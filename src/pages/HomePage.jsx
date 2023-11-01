5a139576e0155c4cf52884b2587756efa344482a
armorbita <armorbita@gmail.com>
2023-10-31 22:59:43 -0800

    local commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import '../assets/scss/HomePage/_homePage.scss';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,6 +7,7 @@ import Slider from "react-slick";
 import mainSliderImg1 from '../assets/img/home-slider.jpg';
 import mainSliderVidio1 from '../assets/video/slider-video.mp4';
 import wolfImg from '../assets/img/image_shape_01.jpg';
+import onboardImg from '../assets/img/image_shape_02.jpg';
 import { FiAnchor } from 'react-icons/fi';
 import { FiUsers } from 'react-icons/fi';
 import { PiUsersThreeBold } from 'react-icons/pi';
@@ -15,11 +16,31 @@ import { MdSpeed } from 'react-icons/md';
 import { AiOutlineCalendar } from 'react-icons/ai';
 import { FaRegListAlt } from 'react-icons/fa';
 import { FaOilCan } from 'react-icons/fa';
-
+import { BsCheckLg } from 'react-icons/bs';
+import { BsFillBookmarkFill } from 'react-icons/bs';
 
 const HomePage = () => {
 
+    const [animeLine, setanimeLine] = useState(0);
+
+    const porstRef = useRef(null);
     const videoRef = useRef(null);
+
+    const handleScroll = () => {
+        if (window.innerWidth > 991) {
+            if (window.scrollY >= porstRef.current.offsetTop - 400 && window.scrollY <= porstRef.current.offsetTop) {
+                setanimeLine((window.scrollY - 2100) / 8)
+            }
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     const settings = {
         fade: true,
         infinite: true,
@@ -68,21 +89,21 @@ const HomePage = () => {
                         <div className="book_block">
                             <div className="block_title icon-map-icon">Our destinations</div>
                             <div className="block_description">
-                                Our base in Volos Marina is easily accessible and is also a 
+                                Our base in Volos Marina is easily accessible and is also a
                                 great starting point for your sailing trip.
                             </div>
                         </div>
                         <div className="book_block">
                             <div className="block_title icon-calendar">Travel calendar</div>
                             <div className="block_description">
-                                Season starts from the month of April and lasts until end of 
+                                Season starts from the month of April and lasts until end of
                                 October. High season is from July to August
                             </div>
                         </div>
                         <div className="book_block">
                             <div className="block_title icon-calling">Easy booking</div>
                             <div className="block_description">
-                                We cover your every step, from your first booking request 
+                                We cover your every step, from your first booking request
                                 with our agents, to your official check-out.
                             </div>
                         </div>
@@ -106,44 +127,44 @@ const HomePage = () => {
                         <div className="wolf_table">
                             <div className="table_inner">
                                 <div className="table_block">
-                                    <FiUsers /> 
+                                    <FiUsers />
                                     <span className='table-name'>GUESTS SLEEPING</span>
                                     <span className='table-number'>12</span>
                                 </div>
                                 <div className="table_block">
-                                    <FiAnchor /> 
+                                    <FiAnchor />
                                     <span className='table-name'>CABINS</span>
                                     <span className='table-number'>6</span>
                                 </div>
                                 <div className="table_block">
-                                    <PiUsersThreeBold /> 
+                                    <PiUsersThreeBold />
                                     <span className='table-name'>CREW</span>
                                     <span className='table-number'>8</span>
                                 </div>
                                 <div className="table_block">
-                                    <FaRuler /> 
+                                    <FaRuler />
                                     <span className='table-name'>LENGTH</span>
                                     <span className='table-number'>42 m</span>
                                 </div>
                             </div>
                             <div className="table_inner">
                                 <div className="table_block">
-                                    <MdSpeed /> 
+                                    <MdSpeed />
                                     <span className='table-name'>CRUISING SPEED</span>
                                     <span className='table-number'>12 kt</span>
                                 </div>
                                 <div className="table_block">
-                                    <AiOutlineCalendar /> 
+                                    <AiOutlineCalendar />
                                     <span className='table-name'>YEARS</span>
                                     <span className='table-number'>08/18</span>
                                 </div>
                                 <div className="table_block">
-                                    <FaRegListAlt /> 
+                                    <FaRegListAlt />
                                     <span className='table-name'>BUILDER</span>
                                     <span className='table-number'>Mondomarine</span>
                                 </div>
                                 <div className="table_block">
-                                    <FaOilCan /> 
+                                    <FaOilCan />
                                     <span className='table-name'>WATER TANK</span>
                                     <span className='table-number'>1600 l</span>
                                 </div>
@@ -155,17 +176,98 @@ const HomePage = () => {
                     </div>
                 </div>
             </div>
-            <div className="ports_section">
+            <div className="ports_section" ref={porstRef}>
                 <div className="custom_container">
                     <div className="title_wrapper center_mode">
                         <div className="section_title">DESTINATIONS</div>
                         <div className="main_title">Ports & Harbours</div>
                         <div className="main_description">
-                            Follow suggested itineraries or make your own 
+                            Follow suggested itineraries or make your own
                             schedule and discover Kefalonia island at your own pace.
                         </div>
                         <div className="background_title">tour</div>
                     </div>
+                    <div className="ports_wrapper">
+                        <span className='animated-Blue-Line' style={{ transform: `translateY(${animeLine}px)` }} ></span>
+                        <ul className="ports_list">
+                            <li className='icon-map'><span className='day-span'>DAY  01</span><div className='port-name'>Sami</div></li>
+                            <li className='icon-map'><span className='day-span'>DAY  02</span><div className='port-name'>Vathy</div></li>
+                            <li className='icon-map'><span className='day-span'>DAY  03</span><div className='port-name'>Atokos</div></li>
+                            <li className='icon-map'><span className='day-span'>DAY  04</span><div className='port-name'>Kalamos</div></li>
+                            <li className='icon-map'><span className='day-span'>DAY  05</span><div className='port-name'>Syvota</div></li>
+                            <li className='icon-map'><span className='day-span'>DAY  06</span><div className='port-name'>Vassiliki</div></li>
+                            <li className='icon-map'><span className='day-span'>DAY  07</span><div className='port-name'>Sami</div></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="onboard_section">
+                <div className="custom_container">
+                    <div className="board_block">
+                        <div className="title_wrapper">
+                            <div className="section_title">SEA WOLF</div>
+                            <div className="main_title">Onboard Amenities</div>
+                            <div className="background_title">yaht</div>
+                        </div>
+                        <ul className="onboard_list">
+                            <li><BsCheckLg />Air conditioning</li>
+                            <li><BsCheckLg />Bimini top</li>
+                            <li><BsCheckLg />Electric toilet</li>
+                            <li><BsCheckLg />Autopilot</li>
+                            <li><BsCheckLg />Dinghy</li>
+                            <li><BsCheckLg />Refrigerator</li>
+                        </ul>
+                        <div className="site_btn">book now <span className='icon-right'></span></div>
+                    </div>
+                    <div className="image_container">
+                        <div className="image_block">
+                            <img src={onboardImg} alt="wolf-img" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="tour_section">
+                <div className="custom_container">
+                    <div className="title_wrapper center_mode">
+                        <div className="section_title">SAILING TOURS</div>
+                        <div className="main_title">June ’23 Calendar</div>
+                        <div className="main_description">Sami – Fiskardo – Kioni – Vathy – Atokos – Kalamos – Spartochori – Syvota – Vassiliki – Sami</div>
+                        <div className="background_title">booking</div>
+                    </div>
+                    <ul className="calendar_list">
+                        <li>
+                            <div className='flag-wrapper'><BsFillBookmarkFill />03 JUN</div>
+                            <div className="block_info">
+                                <div className="block_title">Sami, Kefalonia Booked</div>
+                                <div className="block_price">€ 7.980</div>
+                                <div className="block_date">03.06. - 10.06.2023</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='flag-wrapper'><BsFillBookmarkFill />10 JUN </div>
+                            <div className="block_info">
+                                <div className="block_title"></div>
+                                <div className="block_price">€ 6500</div>
+                                <div className="block_date">03.06. - 09.03.2023</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='flag-wrapper'><BsFillBookmarkFill />17 JUN </div>
+                            <div className="block_info">
+                                <div className="block_title"></div>
+                                <div className="block_price">€ 6.950</div>
+                                <div className="block_date">03.05. - 10.11.2023</div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='flag-wrapper'><BsFillBookmarkFill />24 JUN </div>
+                            <div className="block_info">
+                                <div className="block_title"></div>
+                                <div className="block_price">€ 8.100</div>
+                                <div className="block_date">03.06. - 10.08.2023</div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
